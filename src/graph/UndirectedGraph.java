51ad76d039cc8593d2127b05a00c1bb3f89678bc
agronowski9346 <agronowski9346@gmail.com>
2018-06-10 13:03:16 -0700

    Fix graph bugs

--- src/graph/UndirectedGraph.java
+++ src/graph/UndirectedGraph.java
@@ -24,13 +24,13 @@ public class UndirectedGraph {
 	
 	//Performs a BreadthFirstSearch on the graph starting from a position in the matrix
 	public void breadthFirstSearch(int startVertex) {
-		LinkedList<T> queue = new LinkedList<T>();
+		//LinkedList<T> queue = new LinkedList<T>();
 		int sizeOfAdjacencyList = this.adjacencyMatrix.length;
 		boolean[] visitedVertices= new boolean[sizeOfAdjacencyList];
 	//	queue.offer(this.adjacencyMatrix[positionX][positionY]);
 		visitedVertices[startVertex] = true;
 		
-		System.out.println(vertices.get(queue.poll()));
+		//System.out.println(vertices.get(queue.poll()));
 		//loop through each vertex adjacent to the startVertex and keep going
 		
 	}
