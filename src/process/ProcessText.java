e94ca8843c1385205e1c9cac5c3d56c063907903
agronowski9346 <agronowski9346@gmail.com>
2018-06-10 12:58:30 -0700

    Load Model

--- src/process/ProcessText.java
+++ src/process/ProcessText.java
@@ -1,22 +1,34 @@
 package process;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import opennlp.tools.cmdline.postag.POSModelLoader;
 import opennlp.tools.cmdline.postag.POSTaggerTool;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
 import opennlp.tools.tokenize.SimpleTokenizer;
 
 public class ProcessText {
 	private String fileContents;
 	private String[] tokenizedText;
+	private final String POS_MODEL_LOCATION = "src\\apache-opennlp-1.8.4-bin\\apache-opennlp-1.8.4\\nlpPOSModel\\en-pos-maxent.bin";
 	public ProcessText(String fileContents) {
 		this.setFileContents(fileContents);
 		SimpleTokenizer simpleTokenizer = SimpleTokenizer.INSTANCE;
 		this.tokenizedText = simpleTokenizer.tokenize(fileContents);
-		
 	}
 	
 	//assigns part of speech tags to the text
 	public void POStag() {
-		POSTaggerTool tool = new POSTaggerTool();
-		System.out.println(tool.);
+		try {
+		POSModel model =  new POSModelLoader().load(new File(POS_MODEL_LOCATION));
+		POSTaggerME tagger = new POSTaggerME(model);
+		String[] tags = tagger.tag(this.tokenizedText);
+		for(String test: tags) System.out.println(test);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public String getFileContents() {
