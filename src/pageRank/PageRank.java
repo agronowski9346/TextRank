ecce4a6db974f178f21ecd3310609458e9eaeffa
agronowski9346 <agronowski9346@gmail.com>
2018-06-24 08:37:58 -0700

    added PageRank fields

--- src/pageRank/PageRank.java
+++ src/pageRank/PageRank.java
@@ -1,7 +1,21 @@
 package pageRank;
+
+import graph.UndirectedGraph;
+
 /*
  * applies PageRank on undirected unweighted and weighted graphs.
  */
 public class PageRank {
-
+	//value of the dampening factor usually set to 0.85
+	private final double d = 0.85;
+	private UndirectedGraph graph;
+	
+	public PageRank(UndirectedGraph graph) {
+		this.graph = graph;
+	}
+	public double score(int vertex) {
+		double scoreAtInput = 0;
+		scoreAtInput = (1-d) + 
+		for (int )
+	}
 }
